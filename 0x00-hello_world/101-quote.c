752c7787d065a25fa2be87fbbcb10635aae0be53
Xlroo <victorkenneth.vk@students.uonbi.ac.ke>
2023-10-05 14:12:33 -0700

    print to stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	fprintf(sterr, "nd that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
