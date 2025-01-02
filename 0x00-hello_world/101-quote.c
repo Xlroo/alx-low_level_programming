5af043c0ae80446d4b23e4c413ec2341655d3644
drik3s <>
2025-01-02 13:15:30 +0300

    change strlen function with hardcoded value

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -14,7 +14,7 @@ int main(void)
 
     char *buffer = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-    int bytesWritten = write(2, buffer, strlen(buffer));
+    int bytesWritten = write(2, buffer, 58);
     if (bytesWritten == -1)
     {
 	    return(1);
