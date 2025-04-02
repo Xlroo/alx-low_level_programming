642f19ebb865dd0bbc0ebf5ffae1231047b4304b
Xlroo <80978847+Xlroo@users.noreply.github.com>
2025-04-02 13:49:36 +0300

    Use system call

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <string.h>
 #include "main.h"
 /**
  * main - Entry point
@@ -9,19 +7,14 @@
 
 int main(void)
 {
-	int n;
-	char *s;
-	int i;
-
-	s = "_putchar";
-	n = strlen(s);
-	i = 0;
-
-	while (i <= n)
-	{
-		_putchar(s[i]);
-		i++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
