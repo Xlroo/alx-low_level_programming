ddc775c9a2680a60640fe278d5bd908866ee9cbc
Xlroo <80978847+Xlroo@users.noreply.github.com>
2025-04-07 13:16:59 +0300

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_isalpha - checks if character is an alphabet
+ *@c: character to check
+ *
+ *Return: 1 if alphabet , 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
